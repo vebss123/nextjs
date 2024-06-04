d62e9e39bb7c83380e7366a552afd01a867201f4
Vaibhav Wanare <vaibhavwanare007@gmail.com>
2024-06-04 18:08:04 +0530

    second commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <p>This is a sample Next.js application.</p>
+    </div>
+  );
+};
+
+export default HomePage;
